898d0e9a7d04438ebe3eb468a64cc4f6e2f72c51
Ushinji <siguma971@gmail.com>
2019-11-03 22:33:27 +0900

    Add page size

--- app/controllers/users/pdfs_controller.rb
+++ app/controllers/users/pdfs_controller.rb
@@ -4,6 +4,6 @@ class Users::PdfsController < ApplicationController
   def index
     @users = User.all
     builder = UserPdfBuilder.new(@users)
-    send_data(builder.build, builder.options)
+    send_data(builder.build(params[:page_size]), builder.options)
   end
 end
