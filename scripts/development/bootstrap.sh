ec5ffbe2ec684acfbe19afa3cc988a79b1d0263c
Ushinji <siguma971@gmail.com>
2019-11-20 17:38:06 +0900

    bootstrapスクリプトにyarn installを追加

--- scripts/development/bootstrap.sh
+++ scripts/development/bootstrap.sh
@@ -7,5 +7,6 @@ docker-compose build
 docker-compose run --rm app bundle install
 docker-compose run --rm app bin/rails db:create
 docker-compose run --rm app bin/rails db:migrate
+docker-compose run --rm app yarn install --check-files
 
 docker-compose up -d
