4941865ec64fa3cea68c37893050d0425ef4ef93
Ushinji <siguma971@gmail.com>
2019-12-15 11:16:20 +0900

    Add initialize snapshot pdf.

--- spec/support/custom_matchers/match_pdf_snapshot.rb
+++ spec/support/custom_matchers/match_pdf_snapshot.rb
@@ -4,6 +4,7 @@ require "rspec/expectations"
 
 RSpec::Matchers.define :match_pdf_snapshot do |expected|
   match do |actual|
+    return true if snapshot_not_exist? && generate_snapshot!
     actual_pdf = MiniMagick::Image.read(actual)
     expected_pdf = MiniMagick::Image.open(snapshot_path)
     diff(actual_pdf, expected_pdf)
@@ -21,5 +22,18 @@ RSpec::Matchers.define :match_pdf_snapshot do |expected|
 
   def snapshot_path
     File.join(Rails.root, "spec/fixtures/snapshots", "#{expected}.pdf")
+  end
+
+  def snapshot_not_exist?
+    !File.exist?(snapshot_path)
+  end
+
+  def generate_snapshot!
+    save_pdf!(snapshot_path, actual)
+    report_message("Generate: #{snapshot_path}")
+  end
+
+  def report_message(message)
+    RSpec.configuration.reporter.message(message)
   end
 end
