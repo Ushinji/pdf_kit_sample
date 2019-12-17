c56b6604540c956bf4c52ba4876575d6777c9952
Ushinji <siguma971@gmail.com>
2019-12-17 12:14:05 +0900

    Add save of faliure actual pdf.

--- spec/support/custom_matchers/match_pdf_snapshot.rb
+++ spec/support/custom_matchers/match_pdf_snapshot.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "rspec/expectations"
+require 'fileutils'
 
 RSpec::Matchers.define :match_pdf_snapshot do |expected|
   match do |actual|
@@ -8,7 +9,9 @@ RSpec::Matchers.define :match_pdf_snapshot do |expected|
     actual_pdf = MiniMagick::Image.read(actual)
     expected_pdf = MiniMagick::Image.open(snapshot_path)
     @diff_pages = diff_pages(actual_pdf, expected_pdf)
-    @diff_pages.empty?
+    return true if @diff_pages.empty?
+    save_failure_actual_pdf!
+    false
   end
 
   def diff_pages(actual_pdf, expected_pdf)
@@ -36,6 +39,20 @@ RSpec::Matchers.define :match_pdf_snapshot do |expected|
   def generate_snapshot!
     save_pdf!(snapshot_path, actual)
     report_message("Generate: #{snapshot_path}")
+  end
+
+  def tmp_dir
+    path = Pathname.new(File.join(Rails.root, "tmp/snapshots"))
+    FileUtils.mkdir_p(path) unless path.exist?
+    path
+  end
+
+  def failure_actual_path
+    File.join(tmp_dir, "failures_snapshot_#{expected}_.pdf")
+  end
+
+  def save_failure_actual_pdf!
+    save_pdf!(failure_actual_path, actual)
   end
 
   def report_message(message)
